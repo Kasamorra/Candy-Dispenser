02dcbe6e1d53dd214f29162a0c7c9635f628ba04
Kasamorra <81055522+Kasamorra@users.noreply.github.com>
2021-12-09 13:19:37 +0100

    Commenting the code

--- CandyCodeUnfinished.ino
+++ CandyCodeUnfinished.ino
@@ -1,5 +1,5 @@
 //Candy Dispenser with three seperate containers and button for candy selection
-//Step 4: Trying to optimize the code by attaching an interrupt to the pushbutton
+//Components: 3x Servo Motor, 3x LED, 3x 330 Ohm Resistor, Pushbutton, Ultrasonic Sensor
 //Pins Servos: 9, 10, 13
 //Pins LEDs: 8, 7, 6
 //Pin Pushbutton: 2
@@ -11,16 +11,16 @@ const int servoOpen = 90; //Angle of Servo when dispenser open
 const int servoClosed = 0; //Angle of Servo when dispenser closed
 
 //Servo One
-const int pinS1 = 13; //Pin connected to first servo motor
-Servo servo1; //creates an object of type Servo
+const int pinS1 = 13;
+Servo servo1; 
 
 //Servo Two
-const int pinS2 = 10; //Pin connected to second servo motor
-Servo servo2; //creates an object of type Servo
+const int pinS2 = 10;
+Servo servo2; 
 
 //Servo Three
-const int pinS3 = 9; //Pin connected to third servo motor
-Servo servo3; //creates an object of type Servo
+const int pinS3 = 9; 
+Servo servo3; 
 
 //LEDs
 const int pinLED1 = 8;
@@ -33,24 +33,25 @@ volatile int buttonStateOld = 1; // 0 = pressed, 1 = not pressed
 volatile int buttonStateNew;
 
 //Ultrasonic Sensor
-const int trigPin = 11;
-const int echoPin = 12;
+const int trigPin = 11; //sends Ping
+const int echoPin = 12; //receives Ping
 int pingTime; //time the Ping needs to travel
+const int microDelay = 10; 
 
 //General
-volatile int candyMode = 0; // 0 - 1 - 2
-const int candyLED[] = {pinLED1, pinLED2, pinLED3};
-const int candyOff1[] = {pinLED2, pinLED3, pinLED1};
-const int candyOff2[] = {pinLED3, pinLED1, pinLED2};
-const int openTime = 2000;
+volatile int candyMode = 0; // 0 - 1 - 2, used to choose between different candy containers
+const int candyLED[] = {pinLED1, pinLED2, pinLED3}; //LED of chosen container
+const int candyOff1[] = {pinLED2, pinLED3, pinLED1}; //LED of one of the non-chosen containers
+const int candyOff2[] = {pinLED3, pinLED1, pinLED2}; //LED of the other non-chosen container 
+const int openTime = 2000; //time the dispenser stays open
 const int delayTime = 150; //increase for troubleshooting
-const int microDelay = 10;
 
-//Interrupt
+//Interrupt -> gets called every time button gets pressed and released
 void buttonISR()
 {
   candyMode = (candyMode + 1) % 3;
 }
+
 
 void setup() {
   Serial.begin(9600); //for Troubleshooting
@@ -76,6 +77,7 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(pinButton), buttonISR, RISING);
 }
 
+
 void loop() {
 
   //reset
@@ -98,27 +100,27 @@ void loop() {
 
   Serial.println(pingTime);
 
-  //checking if we should dispense candy
+  //checking if candy is to be dispensed
   if (pingTime < 250)
   {
     switch (candyMode)
     {
-      case 0: //First Mode
+      case 0: //Opens first Candy Container
         servo1.write(servoOpen);
         delay(openTime);
-        servo1.write(servoClosed);
+        servo1.write(servoClosed); //closes container again
         break;
 
-      case 1: //Second Mode
+      case 1: //Opens second Candy Container
         servo2.write(servoOpen);
         delay(openTime);
-        servo2.write(servoClosed);
+        servo2.write(servoClosed); //closes container again
         break;
 
-      case 2: //Third Mode
+      case 2: //Opens third Candy Container
         servo3.write(servoOpen);
-        delay(openTime);
-        servo3.write(servoClosed);
+        delay(openTime); 
+        servo3.write(servoClosed); //closes container again
         break;
     }
   }
